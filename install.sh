74a79d88c85c1ad7af89dbce650bce26e1661934
David Warkentin <ev0rtex@gmail.com>
2019-11-22 15:11:40 -0700

    Use a patch file instead of a whole source file and do a local install

--- install.sh
+++ install.sh
@@ -1,30 +1,30 @@
-coreutils_version="coreutils-8.2"
+COREUTILS_VER=8.29
 
-case $OSTYPE in
-    darwin*)
-        macOS_config_params="gl_cv_func_stpncpy=yes"
-        download_method="curl -o"
-        install_location="/usr/local/bin"
-        ;;
-    *)
-        download_method="wget -O"
-        install_location="/usr/bin"
-        ;;
-esac
-
-$download_method ls_colors_generator.py https://raw.githubusercontent.com/mnurzia/even-better-ls/master/ls_colors_generator.py
+#
+# Copy over the colors generator
 chmod 755 ls_colors_generator.py
-sudo cp ls_colors_generator.py ${install_location}/ls_colors_generator
-$download_method ${coreutils_version}.tar.xz https://ftp.gnu.org/gnu/coreutils/${coreutils_version}.tar.xz
-tar -xf ${coreutils_version}.tar.xz
-rm ${coreutils_version}.tar.xz
-cd ${coreutils_version}/src
-rm -rf ls.c
-$download_method ls.c https://raw.githubusercontent.com/mnurzia/even-better-ls/master/ls.c
-cd ..
-./configure $macOS_config_params
+sudo cp ls_colors_generator.py /usr/bin/ls_colors_generator
+
+#
+# Cleanup
+[ -d "coreutils-${COREUTILS_VER}" ] && \
+    rm -rf coreutils-${COREUTILS_VER}
+[ -f "coreutils-${COREUTILS_VER}.tar.xz" ]  && \
+    rm -f coreutils-${COREUTILS_VER}.tar.xz
+
+#
+# Download coreutils and patch
+wget https://ftp.gnu.org/gnu/coreutils/coreutils-${COREUTILS_VER}.tar.xz
+tar -xf coreutils-${COREUTILS_VER}.tar.xz
+rm coreutils-${COREUTILS_VER}.tar.xz
+cd coreutils-${COREUTILS_VER}
+patch -p0 < ../ls.patch
+
+#
+# Compile and copy binaries
+./configure
 make
-cd src
-sudo mv ls ${install_location}/ls-i
-sudo mv dir ${install_location}/dir-i
-sudo mv vdir ${install_location}/vdir-i
+for bin in ls dir vdir; do
+    echo "Copying ${bin} to /usr/bin/${bin}-i"
+    sudo cp src/${bin} /usr/bin/${bin}-i
+done
